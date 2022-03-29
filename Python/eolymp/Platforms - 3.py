b88bb892c62a5b3259ce620797f9c7b5716d8d51
Youssef Elkilany <13850450+YoussefElkilaney@users.noreply.github.com>
2022-03-30 01:01:20 +0200

    Create Platforms - 3.py

---
+++ Python/eolymp/Platforms - 3.py
@@ -0,0 +1,86 @@
+# https://www.eolymp.com/en/problems/806
+
+from io import BytesIO, IOBase
+import os
+import sys
+
+BUFSIZE = 8192
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = 'x' in file.mode or 'r' not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b: break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b'\n') + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+try: sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+except: pass
+
+def input(): return sys.stdin.readline().rstrip("\r\n")
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mp(): return map(int, sys.stdin.readline().split())
+def inp(): return int(sys.stdin.readline())
+def prl(n): return sys.stdout.write(str(n)+'\n')
+def pr(n): return sys.stdout.write(str(n)+' ')
+
+def solve():
+    n = int(input())
+    A = list(map(int, input().split()))
+    
+    dp = [0] * n
+    dp[1] = abs(A[1]-A[0])**2
+    
+    if len(A) > 2:
+        dp[1] = min(dp[1], 3*abs(A[0]-A[2])**2+abs(A[2]-A[1])**2)
+    
+    for i in range(2, n):
+        dp[i] = min( abs(A[i-1]-A[i])**2 + dp[i-1],
+                    3*abs(A[i-2]-A[i])**2 + dp[i-2] )
+        if i+1 < n:
+            dp[i] = min(dp[i], 3*abs(A[i-1]-A[i+1])**2 + abs(A[i+1]-A[i])**2 + dp[i-1])
+    
+    print(dp[n-1])
+
+def main():
+    T = 1
+#     T = inp()
+    for _ in range(T):
+        solve()
+
+if __name__ == "__main__":
+    main()
